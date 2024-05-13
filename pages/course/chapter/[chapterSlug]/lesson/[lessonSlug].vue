8637642dfd1ea44545fe6b1aec61d18c7e4070f0
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-05-13 23:34:32 +0100

    display lesson content

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -16,8 +16,27 @@ const lesson = computed(() => {
 </script>
 
 <template>
-  <article>
-      <h2>Lesson</h2>
-      <p>{{ lesson?.title }}</p>
-  </article>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter?.number }} - {{ lesson?.number }}
+    </p>
+    <h2 class="my-0">{{ lesson?.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+      v-if="lesson?.sourceUrl"
+      class="font-normal text-md text-gray-500"
+       :href="lesson.sourceUrl"
+       >
+       Download Source Code
+      </a>
+      <a 
+      v-if="lesson?.downloadUrl"
+      class="font-normal text-md text-gray-500"
+      :href="lesson.downloadUrl"
+      >
+      Download Video
+    </a>
+    </div>
+    <p>{{ lesson?.text }}</p>
+  </div>
 </template>
