1291030356e3c0f6c29cca97d699e76bc7806669
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-05-13 23:23:09 +0100

    use of course data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
+const course = useCourse()
 const route = useRoute()
 
+const chapter = computed(() => {
+return course.chapters.find(
+  (chapter) => chapter.slug === route.params.chapterSlug
+);
+});
+
+const lesson = computed(() => {
+  return chapter.value?.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
 </script>
 
 <template>
   <article>
-      <h1>Chapter: {{ route.params.chapterSlug }}</h1>
-      <h2>Lesson: {{ route.params.lessonSlug }}</h2>
-      <p>This is a lesson</p>
+      <h2>Lesson</h2>
+      <p>{{ lesson?.title }}</p>
   </article>
 </template>
