ae32d80dfbfcd219ff43361b81cc010c993183a5
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-05-13 23:07:50 +0100

    created a dynamic lesson path

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+
+</script>
+
+<template>
+  <article>
+      <h1>Chapter: {{ route.params.chapterSlug }}</h1>
+      <h2>Lesson: {{ route.params.lessonSlug }}</h2>
+      <p>This is a lesson</p>
+  </article>
+</template>
