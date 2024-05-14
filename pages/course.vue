e25e30630c191cd18ab04d4cd99e8342272fc40e
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-14 12:22:48 +0200

    added chapters to sidebar

--- pages/course.vue
+++ pages/course.vue
@@ -1,22 +1,43 @@
+<script setup lang="ts">
+const { chapters } = useCourse();
+</script>
+
 <template>
-    <div class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center">
-        <div class="prose mb-12">
-            <h1>
-                <span class="font-medium">
-                    Skill<span class="font-bold">Wave</span>
-                </span>
-            </h1>
-        </div>
-
-        <div class="flex flex-row justify-center flex-grow">
-            <div class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col">
-                <h3>Chapters</h3>
-                <!-- All the lessons for the course go here -->
-            </div>
-
-            <div class="prose p-12 bg-white rounded-md w-[65ch]">
-                <NuxtPage />
-            </div>
-        </div>
+  <div
+    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
+  >
+    <div class="prose mb-12">
+      <h1>
+        <span class="font-medium">
+          Skill<span class="font-bold">Wave</span>
+        </span>
+      </h1>
     </div>
+
+    <div class="flex flex-row justify-center flex-grow">
+      <div
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
+      >
+        <h3>Chapters</h3>
+
+        <!-- Chapters -->
+        <div v-for="chapter in chapters" :key="chapter.slug">
+          <h4>{{ chapter.title }}</h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
+          >
+            <span class="text-gray-500">{{ index + 1 }}.</span>
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
+        <!-- / Chapters -->
+      </div>
+
+      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+        <NuxtPage />
+      </div>
+    </div>
+  </div>
 </template>
