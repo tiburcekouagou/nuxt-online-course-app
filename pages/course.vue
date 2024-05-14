4775f80a174025dda7f615a8db2cb75433ab4801
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-14 14:26:23 +0200

    change to amber bg

--- pages/course.vue
+++ pages/course.vue
@@ -9,7 +9,7 @@ const isActiveLink = (lessonPath: string): boolean => {
 
 <template>
   <div
-    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
+    class="p-12 bg-amber-400 w-full h-full min-h-screen flex flex-col items-center"
   >
     <div class="prose mb-12">
       <h1>
