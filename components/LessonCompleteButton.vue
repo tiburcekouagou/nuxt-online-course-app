b840b26e0030b139dea4d91ede376dfa98e261e2
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-14 13:53:26 +0200

    mark the course as completed

---
+++ components/LessonCompleteButton.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+type Props = {
+  modelValue: boolean;
+};
+
+const props = withDefaults(defineProps<Props>(), { modelValue: false });
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: boolean): void
+}>()
+</script>
+
+<template>
+  <label 
+  class="rounded text-white font-bold py-2 px-4 cursor-pointer"
+  :class="{
+    'bg-green-500': modelValue,
+    'bg-gray-500': !modelValue
+  }"
+  >
+    <input type="checkbox" 
+    :value="props.modelValue"
+    @input="() =>emit('update:modelValue', !modelValue)"
+    class="hidden"
+     />
+     {{ modelValue ? 'Completed' : 'Mark as completed' }}
+    </label>
+</template>
+
+<style scoped>
+::selection {
+    display: none;
+}
+</style>
--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -15,30 +15,56 @@ const lesson = computed(() => {
 });
 
 const pageTitle = computed(() => {
-  return `${lesson.value?.title} - ${course.title}` || "Skill Wave"
-})
+  return `${lesson.value?.title} - ${course.title}` || "Skill Wave";
+});
 
 useHead({
-  title: pageTitle
-})
+  title: pageTitle,
+});
+
+const progress = useState<boolean[][]>("progress", () => {
+  return [];
+});
+
+const isLessonComplete = computed(() => {
+  if (chapter.value && lesson.value) {
+    if (!progress.value[chapter.value.number - 1]) {
+      return false;
+    }
+    if (!progress.value[chapter.value.number - 1][lesson.value.number - 1]) {
+      return false;
+    }
+    return progress.value[chapter.value.number - 1][lesson.value.number - 1];
+  }
+});
+
+const toggleComplete = () => {
+  if (chapter.value && lesson.value) {
+    if (!progress.value[chapter.value.number - 1]) {
+      progress.value[chapter.value.number - 1] = [];
+    }
+    progress.value[chapter.value.number - 1][lesson.value.number - 1] =
+      !isLessonComplete.value;
+  }
+};
 </script>
 
 <template>
-  <div>
+  <div v-if="chapter && lesson">
     <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
-      Lesson {{ chapter?.number }} - {{ lesson?.number }}
+      Lesson {{ chapter.number }} - {{ lesson.number }}
     </p>
-    <h2 class="my-0">{{ lesson?.title }}</h2>
+    <h2 class="my-0">{{ lesson.title }}</h2>
     <div class="flex space-x-4 mt-2 mb-8">
       <NuxtLink
-        v-if="lesson?.sourceUrl"
+        v-if="lesson.sourceUrl"
         class="font-normal text-md text-gray-500"
         :to="lesson.sourceUrl"
       >
         Download Source Code
       </NuxtLink>
       <NuxtLink
-        v-if="lesson?.downloadUrl"
+        v-if="lesson.downloadUrl"
         class="font-normal text-md text-gray-500"
         :to="lesson.downloadUrl"
       >
@@ -47,11 +73,16 @@ useHead({
     </div>
 
     <VideoPlayer
-      v-if="lesson?.videoId"
-      :video-id="lesson?.videoId"
+      v-if="lesson.videoId"
+      :video-id="lesson.videoId"
       class="w-full"
     />
 
-    <p>{{ lesson?.text }}</p>
+    <p>{{ lesson.text }}</p>
+    <LessonCompleteButton
+      :model-value="isLessonComplete"
+      @update:model-value="toggleComplete"
+    />
   </div>
+  <div v-else>No content found !</div>
 </template>
