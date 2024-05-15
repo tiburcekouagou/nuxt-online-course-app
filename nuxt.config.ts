b7ccc862f53e9d4f6c1d26cc84e67c9f96ffbb65
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-05-15 19:02:58 +0200

    implemented login

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
   modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/supabase"],
   typescript: {
     typeCheck: true
+  },
+  supabase: {
+    url: process.env.SUPABASE_URL
+  },
+  runtimeConfig: {
+    public: {
+      appUrl: process.env.APP_URL || 'http://localhost:3000'
+    }
   }
 })
---
+++ pages/confirm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Waiting for login...
+  </div>
+</template>
+
+<script lang="ts" setup>
+const user = useSupabaseUser();
+
+watch(user, () => {
+  if (user.value) {
+    // Redirect to protected page
+    return navigateTo('/course')
+  }
+}, { immediate: true })
+</script>
+
+<style></style>
