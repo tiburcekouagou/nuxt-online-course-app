9a260702f7dc4713be3e10c3c6dceb8b2e817ea2
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-16 14:33:21 +0200

    implement lesson get

---
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -0,0 +1,28 @@
+import courseData from "~/server/courseData";
+
+export default defineEventHandler((event) => {
+    const { chapterSlug, lessonSlug } = event.context.params || {};
+
+    const chapter = courseData.chapters.find(
+        (chapter) => chapter.slug === chapterSlug
+    );
+
+    if (!chapter) {
+        throw createError({
+            statusCode: 404,
+            statusMessage: "Not Found",
+            message: "Chapter not found",
+        });
+    }
+
+    const lesson = chapter.lessons.find((lesson) => lesson.slug === lessonSlug)
+
+    if (!lesson) {
+        throw createError({
+            statusCode: 404,
+            statusMessage: "Not Found",
+            message: "Lesson not found",
+        });
+    }
+    return lesson;
+})
