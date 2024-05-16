003677d58ba78598415d7741bb74ea2eb93122df
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-16 18:50:02 +0200

    removed unecessary console.log

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware(({ params, path }, from) => {
     const user = useSupabaseUser();
-    console.log({user: user.value})
     
     if (user.value || params.chapterSlug === "1-chapter-1") {
         return;
