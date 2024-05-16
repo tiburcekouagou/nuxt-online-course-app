e19bb7b3404f002cda12799eaa9de1f8dd0b4132
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-05-16 17:52:34 +0200

    guard and narrow type to union of AppError & Error

--- error.vue
+++ error.vue
@@ -1,34 +1,54 @@
 <script setup lang="ts">
 interface AppError {
-url?: string;
-statusCode?: number;
-statusMessage?: string;
-message?: string;
-description?: string;
-stack?: string;
-data?: any;
+  url?: string;
+  statusCode: number;
+  statusMessage: string;
+  message: string;
+  description: string;
+  stack: string;
+  data?: any;
 }
 
-const error = useError() as Ref<AppError>;
-const handleError = () => {
-    clearError({redirect: "/course"});
+function isAppError(arg: unknown): arg is AppError {
+  return !!arg && typeof arg === "object";
 }
+
+const error = useError();
+if (!isAppError(error.value))
+  throw new Error("Unknown error type or error is undeinfed");
+const {
+  fatal,
+  url,
+  statusCode,
+  statusMessage,
+  message,
+  description,
+  data,
+  stack,
+} = error.value;
+
+const handleError = () => {
+  clearError({ redirect: "/course" });
+};
 </script>
 <template>
-    <NuxtLayout>
-        <div class="prose text-center">
-            <h1>{{ error.statusCode }} - {{ error.statusMessage }}</h1>
-            <h2>{{ error.message }}</h2>
-            <p><strong>URL: </strong> {{ error.url }}</p>
-            <pre v-if="error.stack">{{ error.stack }}</pre>
-            <div class="prose">
-                <p>
-                    Go to
-                    <a
-                        class="hover:cursor-pointer"
-                     @click="handleError">course home</a>
-                </p>
-            </div>
-        </div>
-    </NuxtLayout>
+  <NuxtLayout>
+    <div class="prose text-center">
+      <h1>{{ statusCode }} - {{ statusMessage }}</h1>
+
+      <h2>{{ message }}</h2>
+      <p v-if="url"><strong>URL: </strong> {{ url }}</p>
+      <p v-if="stack">
+          <pre>{{ stack }}</pre>
+      </p>
+      <p v-if="description">{{ description }}</p>
+      <p v-if="data">data: {{ data }}</p>
+      <div class="prose text-center max-w-full">
+        <p>
+          Go to
+          <a class="hover:cursor-pointer" @click="handleError">course home</a>
+        </p>
+      </div>
+    </div>
+  </NuxtLayout>
 </template>
